d1831cf4ca4cf51cb7ca3437144f2efc90cdf461
Jiacheng Zhang <jiachengzhang1@email.arizona.edu>
2020-05-30 22:55:17 -0700

    fix navbar color issue

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,4 +1,6 @@
+import "bootstrap/dist/css/bootstrap.css";
 import "./NavigationBar.css";
+
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { Navbar, Nav } from "react-bootstrap";
